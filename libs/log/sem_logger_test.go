705c6a6e3545db0fe78a843bd5893d20e587f119
Bernd <bemue2@gmx.net>
2023-08-24 17:16:47 +0200

    Added unit tests for ctx loggers

--- libs/log/sem_logger_test.go
+++ libs/log/sem_logger_test.go
@@ -16,7 +16,7 @@ func addRule(t *testing.T, logger log.Logger, entity log.SemEntity, rule string)
 	seml.AddRule(log.SemTransaction, []byte(rule))
 }
 
-// helper to set a SEM logger in active state
+// helper to set a SEM logger in active state (add rule & enter matching that rule)
 func goActive(t *testing.T, logger log.Logger) {
 	seml, ok := logger.(log.SemLogger)
 	require.True(t, ok, "Bad test setup. Expected a SEM logger but it isn't")
@@ -116,15 +116,17 @@ func TestSemWithTMLogger(t *testing.T) {
 
 func TestSemWithWith(t *testing.T) {
 
-	t.Run("Logger with module and user context but no restrictions",
+	t.Run("Logger with module and user context all fiters allowed",
 		func(t *testing.T) {
 			var buf bytes.Buffer
 			baselogger := log.NewTMJSONLoggerNoTS(&buf)
 			filtered := log.NewFilter(baselogger, log.AllowAll())
-
 			semlogger := log.NewSEM(filtered)
-			sem1 := semlogger.With("module", "mymod", "user", "Sam")
 
+			// Create contextual logger from SEM logger
+			semWithLogger := semlogger.With("module", "mymod", "user", "Sam")
+
+			// Ensure original output is as expected
 			semlogger.Debug("here", "this is", "debug log")
 			semlogger.Info("here", "this is", "info log")
 			semlogger.Error("here", "this is", "error log")
@@ -139,22 +141,11 @@ func TestSemWithWith(t *testing.T) {
 				t.Errorf("\nwant:\n%s\nhave:\n%s", want, have)
 			}
 
-			//	    logger = log.NewFilter(logger, log.AllowError(), log.AllowInfoWith("module", "crypto"))
-			//			 logger.With("module", "crypto").Info("Hello") # produces "I... Hello module=crypto"
-			//
-			//	    logger = log.NewFilter(logger, log.AllowError(),
-			//					log.AllowInfoWith("module", "crypto"),
-			//					log.AllowNoneWith("user", "Sam"))
-			//			 logger.With("module", "crypto", "user", "Sam").Info("Hello") # returns nil
-			//
-			//	    logger = log.NewFilter(logger,
-			//					log.AllowError(),
-			//					log.AllowInfoWith("module", "crypto"), log.AllowNoneWith("user", "Sam"))
-			//			 logger.With("user", "Sam").With("module", "crypto").Info("Hello") # produces "I... Hello module=crypto user=Sam"
+			// Check output of context logger is as expected
 			buf.Reset()
-			sem1.Debug("here", "this is", "debug log")
-			sem1.Info("here", "this is", "info log")
-			sem1.Error("here", "this is", "error log")
+			semWithLogger.Debug("here", "this is", "debug log")
+			semWithLogger.Info("here", "this is", "info log")
+			semWithLogger.Error("here", "this is", "error log")
 
 			want = strings.Join([]string{
 				`{"_msg":"here","level":"debug","module":"mymod","this is":"debug log","user":"Sam"}`,
@@ -166,6 +157,7 @@ func TestSemWithWith(t *testing.T) {
 				t.Errorf("\nwant:\n%s\nhave:\n%s", want, have)
 			}
 
+			// Double check original SEM logger output
 			buf.Reset()
 			semlogger.Debug("here", "this is", "debug log")
 			semlogger.Info("here", "this is", "info log")
@@ -180,4 +172,102 @@ func TestSemWithWith(t *testing.T) {
 				t.Errorf("\nwant:\n%s\nhave:\n%s", want, have)
 			}
 		})
+}
+func TestSemWithCtxFiltering(t *testing.T) {
+	testCases := []struct {
+		name          string
+		active        bool
+		enabled       bool
+		loggerBackend func(io.Writer) log.Logger
+		wantOrig      string
+		wantCtx       string
+	}{
+		{
+			"SEM active with '*:Error, consensus:Info'",
+			true,
+			true,
+			func(w io.Writer) log.Logger {
+				logger := log.NewTMJSONLoggerNoTS(w)
+				return log.NewFilter(logger, log.AllowError(), log.AllowInfoWith("module", "consensus"))
+			},
+			strings.Join([]string{
+				`{"_msg":"here","level":"debug","this is":"debug log"}`,
+				`{"_msg":"here","level":"info","this is":"info log"}`,
+				`{"_msg":"here","level":"error","this is":"error log"}`,
+			}, "\n"),
+			strings.Join([]string{
+				`{"_msg":"here","level":"debug","module":"consensus","this is":"debug log"}`,
+				`{"_msg":"here","level":"info","module":"consensus","this is":"info log"}`,
+				`{"_msg":"here","level":"error","module":"consensus","this is":"error log"}`,
+			}, "\n"),
+		},
+		{
+			"SEM inactive with '*:Error, consensus:Info'",
+			false,
+			true,
+			func(w io.Writer) log.Logger {
+				logger := log.NewTMJSONLoggerNoTS(w)
+				return log.NewFilter(logger, log.AllowError(), log.AllowInfoWith("module", "consensus"))
+			},
+			strings.Join([]string{
+				`{"_msg":"here","level":"error","this is":"error log"}`,
+			}, "\n"),
+			strings.Join([]string{
+				`{"_msg":"here","level":"info","module":"consensus","this is":"info log"}`,
+				`{"_msg":"here","level":"error","module":"consensus","this is":"error log"}`,
+			}, "\n"),
+		},
+
+		{
+			"SEM disabled with '*:Error, consensus:Info'",
+			false,
+			false,
+			func(w io.Writer) log.Logger {
+				logger := log.NewTMJSONLoggerNoTS(w)
+				return log.NewFilter(logger, log.AllowError(), log.AllowInfoWith("module", "consensus"))
+			},
+			strings.Join([]string{
+				`{"_msg":"here","level":"error","this is":"error log"}`,
+			}, "\n"),
+			strings.Join([]string{
+				`{"_msg":"here","level":"info","module":"consensus","this is":"info log"}`,
+				`{"_msg":"here","level":"error","module":"consensus","this is":"error log"}`,
+			}, "\n"),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			filtered := tc.loggerBackend(&buf)
+			semlogger := log.NewSEM(filtered)
+
+			if tc.enabled {
+				addRule(t, semlogger, log.SemAddress, "a Rule")
+			}
+			if tc.active {
+				goActive(t, semlogger)
+			}
+			semCtxLogger := semlogger.With("module", "consensus")
+
+			// Check SEM Ctx logger
+			semCtxLogger.Debug("here", "this is", "debug log")
+			semCtxLogger.Info("here", "this is", "info log")
+			semCtxLogger.Error("here", "this is", "error log")
+			if want, have := tc.wantCtx, strings.TrimSpace(buf.String()); want != have {
+				t.Errorf("\nwant:\n%s\nhave:\n%s", want, have)
+			}
+
+			// Check SEM origin logger
+			buf.Reset()
+			semlogger.Debug("here", "this is", "debug log")
+			semlogger.Info("here", "this is", "info log")
+			semlogger.Error("here", "this is", "error log")
+
+			if want, have := tc.wantOrig, strings.TrimSpace(buf.String()); want != have {
+				t.Errorf("\nwant:\n%s\nhave:\n%s", want, have)
+			}
+		})
+	}
 }
